b494c0204639334e363543c7e4507dcdc7fef939
kentoyArgabio <137607624+kentoyArgabio@users.noreply.github.com>
2023-06-24 17:46:39 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: "media",
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./node_modules/flowbite/**/*.js",
+    ],
+
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    50: "#eff6ff",
+                    100: "#dbeafe",
+                    200: "#bfdbfe",
+                    300: "#93c5fd",
+                    400: "#60a5fa",
+                    500: "#3b82f6",
+                    600: "#2563eb",
+                    700: "#1d4ed8",
+                    800: "#1e40af",
+                    900: "#1e3a8a",
+                },
+            },
+            fontFamily: {
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            backgroundImage: {
+                hero: "url('http://e-tour.com/images/spot1.jpg')",
+            },
+        },
+    },
+    plugins: [require("flowbite/plugin"), require("@tailwindcss/forms")],
+};
